f6c507196e129f472057b133b25259df47a08a17
BrunooHms <Bruno_Hms@outlook.com>
2026-01-08 20:56:27 -0300

    Feat: create dashboard page and configure rote

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { HeaderComponent } from "./core/layout/header/header/header.component";
-import { SidebarCComponent } from "./core/layout/sidebar/sidebar-c/sidebar-c.component";
-import { MainLayoutComponent } from "./core/layout/main-layout/main-layout.component";
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent, SidebarCComponent, MainLayoutComponent],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
